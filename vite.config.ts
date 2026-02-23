6c5b2ae9535227c9d7856021868c91f0c7e8f6da
decastyle <decastyled@gmail.com>
2026-02-23 17:06:29 +0500

    Remove /aktown/ from base

--- vite.config.ts
+++ vite.config.ts
@@ -21,5 +21,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/aktown/" /* TODO: remove when changing domain name to custom one */,
+  base: "/"
 });
