54aa3058c2adda989a16a07c1fb60e341284ff64
decastyle <decastyled@gmail.com>
2026-03-24 06:13:56 +0500

    Add prettier config for opinionated formatting

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+export default {
+    printWidth: 80,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    arrowParens: 'always',
+}
