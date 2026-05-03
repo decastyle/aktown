62c3a5b6d8e1a71c46e2c89b71d2cbad6b2ac4fb
decastyle <decastyled@gmail.com>
2026-05-03 21:06:05 +0500

    Set scroll position to 0 when changing routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,17 @@
-import { createRootRoute, createRoute, Outlet } from '@tanstack/react-router';
+import { createRootRoute, createRoute, Outlet, useRouterState } from '@tanstack/react-router';
 import App from "./App";
 import BirAuyl from "./components/birauyl";
 import { ThemeProvider } from '@/components/theme-provider/theme-provider';
 import { LangProvider } from './i18n';
+import { useEffect } from 'react';
 
 function RootLayout() {
+  const pathname = useRouterState({ select: (s) => s.location.pathname });
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+  }, [pathname]);
+
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <LangProvider>
